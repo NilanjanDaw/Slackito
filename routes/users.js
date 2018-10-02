68908722cfc35bc6d8b31bd0aa2000db3c577325
nilanjandaw <nilanjandaw@gmail.com>
2018-10-02 20:47:52 +0530

    partially added login

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
+const models = require('.././models');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/login', function (req, res, next) {
+  let password = req.body.password
+  let username = req.body.username
+  res.send("")
+})
 
 module.exports = router;
